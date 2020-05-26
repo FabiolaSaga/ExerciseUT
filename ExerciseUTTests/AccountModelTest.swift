ca88cf229eb14cdfacbea4170b08a3a01c6bbd25
FabiolaSaga <saga.fabiola@gmail.com>
2020-05-26 13:43:05 -0500

    Added test for Transact() with XCTAssertEqual

--- ExerciseUTTests/AccountModelTest.swift
+++ ExerciseUTTests/AccountModelTest.swift
@@ -11,25 +11,21 @@ import XCTest
 
 
 class AccountModelTest: XCTestCase {
+    
+    var model: AccountModel?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        model = AccountModel()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testTransact() {
+        let result = model?.transact(deposit: 10, withdraw: 4)
+        XCTAssertEqual(6, result)
     }
 
 }
